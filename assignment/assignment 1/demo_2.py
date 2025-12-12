36e728df020af068d25f3f8fe5e17d2fe1a16593
dhanashri024 <dhanashripowar04@gmail.com>
2025-12-12 13:20:25 +0530

    assignment 1

---
+++ assignment/assignment 1/demo_2.py
@@ -0,0 +1,3 @@
+num1=int(input("enter 1st number: "))
+num2=int(input("enter 2nd number:"))
+print("sum=",num1+num2)
---
+++ assignment/assignment 1/demo_3.py
@@ -0,0 +1,8 @@
+num1=int(input("enter 1st number: "))
+num2=int(input("enter 2nd number:"))
+temp=num1
+num1=num2
+num2=temp
+print("after swapping")
+print("num1",num1)
+print("num2",num2)
